9190206366731f30684d57d031d9ca5c32d4dda0
Will Rossiter <will@fullscreen.io>
2022-05-20 15:34:59 +1200

    fix: unprotect tags if removed from yaml
    Also adds support for marking a tag as unprotected
    
    ```
    tagurit_protected_taxonomy:
      Foods:
        - Pizza: false
    ```

--- src/Tasks/BuildTaxonomyFromConfig.php
+++ src/Tasks/BuildTaxonomyFromConfig.php
@@ -6,10 +6,11 @@ use SilverStripe\Dev\BuildTask;
 use SilverStripe\Core\Config\Config;
 use DNADesign\Tagurit\Model\TaxonomyTerm;
 use DNADesign\Tagurit\Model\TaxonomyType;
+use SilverStripe\Control\Director;
 use SilverStripe\ORM\ValidationException;
 
 /**
- * BuildTask to create the set of protected Types and Terms from config. 
+ * BuildTask to create the set of protected Types and Terms from config.
  * The protected taxonomy is created on build, and missing records are recreated.
  */
 class BuildTaxonomyFromConfig extends BuildTask
@@ -26,60 +27,89 @@ class BuildTaxonomyFromConfig extends BuildTask
     {
         $terms = Config::inst()->get('tagurit_protected_taxonomy');
 
-        if (count($terms) > 0) {
-
+        if ($terms && count($terms) > 0) {
             foreach ($terms as $typename => $terms) {
+                $type = TaxonomyType::get()->find('Name', $typename);
 
-                // Check whether the type already exists,
-                $existingType = TaxonomyType::get()->find('Name', $typename);
-                $type = $existingType ? $existingType : TaxonomyType::create();
-
-                // if it doesn't, create it. 
-                if (is_null($existingType)) {
-                    echo sprintf('Creating taxonomy Type "%s"... ', $typename);
+                if (!$type) {
+                    echo sprintf('Creating taxonomy Type "%s"... ', $typename) . $this->eol();
+                    $type = TaxonomyType::create();
                     $type->Name = $typename;
-                    $type->Protected = true;
-
-                    try {
-                        $type->write();
-                        echo 'done. <br>';
-                    } catch (ValidationException $e) {
-                        echo 'failed. <br>';
-                    }
                 } else {
-                    // if it does, proceed to the terms.
-                    echo sprintf('Found taxonomy Type "%s". <br>', $typename);
+                    echo sprintf('Updating taxonomy Type "%s"... ', $typename) . $this->eol();
                 }
+
+                $type->Protected = true;
+                $type->write();
 
                 if (!$terms) {
                     continue;
                 }
-                
-                foreach ($terms as $term) {
-                    // Check whether the term already exists                    
-                    if (is_null(TaxonomyTerm::get()->find('Name', $term))) {
-                        echo sprintf('Creating Term "%s" in Type "%s"... ', $term, $type->Name);
-                        $newTerm = TaxonomyTerm::create();
-                        $newTerm->Name = $term;
-                        $newTerm->TypeID = $type->ID;
-                        $newTerm->Protected = true;
 
-                        try {
-                            $newTerm->write();
-                            echo 'done. <br>';
-                        } catch (ValidationException $e) {
-                            echo 'failed. <br>';
-                        }
+                $names = $type->Terms()->column('Name');
+                $names = array_combine($names, $names);
+
+                foreach ($terms as $term) {
+                    if (is_array($term)) {
+                        $k = array_key_first($term);
+                        $protected = $term[$k];
+                        $term = $k;
                     } else {
-                        echo sprintf('Found Term "%s". <br>', $typename);
+                        $protected = true;
                     }
 
+                    if (isset($names[$term])) {
+                        unset($names[$term]);
+                    }
+
+                    $termObj = TaxonomyTerm::get()->find('Name', $term);
+
+                    if (!$termObj) {
+                        echo sprintf('  + creating Term "%s" in Type "%s"... ', $term, $type->Name) . $this->eol();
+                        $termObj = TaxonomyTerm::create();
+                        $termObj->Name = $term;
+                    } else {
+                        echo sprintf('  - updating Term "%s" in Type "%s"... ', $term, $type->Name) . $this->eol();
+                    }
+
+                    $termObj->TypeID = $type->ID;
+                    $termObj->Protected = $protected;
+
+                    try {
+                        $termObj->write();
+                    } catch (ValidationException $e) {
+                        echo $e->getMessage() . $this->eol();
+                    }
                 }
-                echo '<br>';
+
+                // we don't have any more protected so make sure they are removed
+                if ($names) {
+                    $clear = $type->Terms()->filter('Name', $names);
+
+                    foreach ($clear as $tag) {
+                        $tag->Protected = false;
+
+                        try {
+                            $tag->write();
+                        } catch (ValidationException $e) {
+                            echo $e->getMessage() . $this->eol();
+                        }
+                    }
+                }
+
+                echo $this->eol();
             }
-            
         } else {
-            echo 'No protected Types defined<br>';
+            echo 'No protected Types defined' . $this->eol();
+        }
+    }
+
+    public function eol()
+    {
+        if (Director::is_cli()) {
+            echo PHP_EOL;
+        } else {
+            echo '<br />';
         }
     }
 }
